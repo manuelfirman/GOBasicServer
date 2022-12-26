a99b766cc951a4d27caddbfcc304dca2260a3e13
Manuel Firman <92244840+manuelfirman@users.noreply.github.com>
2022-12-25 23:51:58 -0300

    1

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fp
+}
+
+func main(){
+	fileServer := http.fileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListendAndServe(":8080", nil); err != nill{
+		log.Fatal(err)
+	}
+
+}
